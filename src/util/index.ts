848e87ae6ce5846a2eb85ea45f2e557ec5e42c2a
Kelly, Jack <jack.kelly@myyellow.com>
2021-04-26 15:37:55 -0500

    Simplified one liner if statements

--- src/util/index.ts
+++ src/util/index.ts
@@ -3,7 +3,5 @@ import * as cookie from "cookie";
 export const getTraceId = (rawCookies: string): string | undefined => {
   const cookies = cookie.parse(rawCookies);
 
-  if (cookies.traceId) {
-    return cookies.traceId;
-  }
+  if (cookies.traceId) return cookies.traceId;
 };
--- src/util/middleware.ts
+++ src/util/middleware.ts
@@ -5,15 +5,9 @@ import { getTraceId } from "./";
 export const requireTraceId: RequestHandler = async function (req, res, next) {
   let traceId: string | undefined;
 
-  if (req.headers.cookie) {
-    traceId = getTraceId(req.headers.cookie);
-  }
+  if (req.headers.cookie) traceId = getTraceId(req.headers.cookie);
 
-  if (traceId) {
-    signale.info(`Trace ID is present: ${traceId}`);
-
-    return next();
-  }
+  if (traceId) return next();
 
   signale.security("Trace ID not present in making log request.");
   res
